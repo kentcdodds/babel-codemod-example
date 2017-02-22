6b43f51df00e1b948711d105fadd32802ed26c12
Kent C. Dodds <kent@doddsfamily.us>
2017-02-22 10:09:46 -0700

    Initial commit

---
+++ plugin.js
@@ -0,0 +1,45 @@
+module.exports = function(babel) {
+  const { types: t } = babel
+  // could just use https://www.npmjs.com/package/is-builtin-module
+  const nodeModules = [
+    'fs', 'path', 'child_process',
+  ]
+
+  return {
+    name: 'node-esmodule', // not required
+    visitor: {
+      ImportDeclaration(path) {
+        const specifiers = []
+        let defaultSpecifier
+        path.get('specifiers').forEach(specifier => {
+          if (t.isImportSpecifier(specifier)) {
+            specifiers.push(specifier)
+          } else {
+            defaultSpecifier = specifier
+          }
+        })
+        const {node: {value: source}} = path.get('source')
+        if (!specifiers.length || !nodeModules.includes(source)) {
+          return
+        }
+        let memberObjectNameIdentifier
+        if (defaultSpecifier) {
+          memberObjectNameIdentifier = defaultSpecifier.node.local
+        } else {
+          memberObjectNameIdentifier = path.scope.generateUidIdentifier(source)
+          path.node.specifiers.push(t.importDefaultSpecifier(memberObjectNameIdentifier))
+        }
+        specifiers.forEach(specifier => {
+          const {node: {imported: {name}}} = specifier
+          const {referencePaths} = specifier.scope.getBinding(specifier.node.imported.name)
+          referencePaths.forEach(refPath => {
+            refPath.replaceWith(
+              t.memberExpression(memberObjectNameIdentifier, t.identifier(name))
+            )
+          })
+          specifier.remove()
+        })
+      }
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import fs, {readFile} from 'fs'
+import {resolve} from 'path'
+import cp from 'child_process'
+
+resolve(__dirname, './thing')
+
+fs.readFile('./thing.js', 'utf8', (err, string) => {
+  console.log(string)
+})
+
+fs.readFile('./other-thing', 'utf8', (err, string) => {
+  const resolve = string => string
+  console.log(resolve())
+})
+
+cp.execSync('echo "hi"')
